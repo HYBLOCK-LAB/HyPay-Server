baf3197afe02807cd18ac762052d307237d974a4
Jaemani <leejaeman0227@gmail.com>
2025-06-05 21:50:11 +0900

    .

--- routes/xumm.js
+++ routes/xumm.js
@@ -12,6 +12,9 @@ router.post('/xumm-sign', async (req, res) => {
         key: process.env.XUMM_API_KEY,
         secret: process.env.XUMM_API_SECRET
       });
+      const { address } = await this.wallet.connect();
+      const { pong } = await this.wallet.ping();
+      console.log(pong)
   
       // Create a payment payload
       const payloadUuid = await wallet.createPaymentTx({ amount });
@@ -33,36 +36,39 @@ router.post('/xumm-sign', async (req, res) => {
 
 // not working
 
-//   router.get('/xumm-sign', async (req, res) => {
-//     try {
-//       const { destination, amount, identifier, instruction, blob } = req.query;
-//       console.log('Received /xumm-sign with:', { destination, amount, identifier, instruction, blob });
-//       if (!destination || !amount) {
-//         return res.status(400).json({ error: 'Missing destination or amount' });
-//       }
-//       const wallet = new XamanWallet(destination, {
-//         key: process.env.XUMM_API_KEY,
-//         secret: process.env.XUMM_API_SECRET
-//       });
-//       const payloadUuid = await wallet.createPaymentTx({
-//         amount,
-//         meta_data: {
-//           identifier,
-//           instruction,
-//           blob
-//         }
-//       });
-//       const payload = await wallet.xumm.payload?.get(payloadUuid);
-//       if (payload && payload.next && payload.next.always) {
-//         return res.redirect(payload.next.always);
-//       } else {
-//         return res.status(500).json({ error: 'Failed to create XUMM payload' });
-//       }
-//     } catch (error) {
-//       console.error('Error creating XUMM sign request:', error);
-//       res.status(500).json({ error: error.message || 'Internal server error', stack: error.stack });
-//     }
-//   });
+  // router.get('/xumm-sign', async (req, res) => {
+  //   try {
+  //     const { destination, amount, identifier, instruction, blob } = req.query;
+  //     console.log('Received /xumm-sign with:', { destination, amount, identifier, instruction, blob });
+  //     if (!destination || !amount) {
+  //       return res.status(400).json({ error: 'Missing destination or amount' });
+  //     }
+  //     const wallet = new XamanWallet(destination, {
+  //       key: process.env.XUMM_API_KEY,
+  //       secret: process.env.XUMM_API_SECRET
+  //     });
+  //     const { address } = await wallet.connect();
+  //     const { pong } = await wallet.ping();
+  //     console.log(pong)
+  //     const payloadUuid = await wallet.createPaymentTx({
+  //       amount,
+  //       meta_data: {
+  //         identifier,
+  //         instruction,
+  //         blob
+  //       }
+  //     });
+  //     const payload = await wallet.xumm.payload?.get(payloadUuid);
+  //     if (payload && payload.next && payload.next.always) {
+  //       return res.redirect(payload.next.always);
+  //     } else {
+  //       return res.status(500).json({ error: 'Failed to create XUMM payload' });
+  //     }
+  //   } catch (error) {
+  //     console.error('Error creating XUMM sign request:', error);
+  //     res.status(500).json({ error: error.message || 'Internal server error', stack: error.stack });
+  //   }
+  // });
 
   const { XummSdk } = require('xumm-sdk');
 
@@ -91,7 +97,7 @@ router.post('/xumm-sign', async (req, res) => {
           }
       
           const sdk = new XummSdk(process.env.XUMM_API_KEY, process.env.XUMM_API_SECRET);
-      
+            
           const payload = {
             TransactionType: 'Payment',
             Destination: destination,
